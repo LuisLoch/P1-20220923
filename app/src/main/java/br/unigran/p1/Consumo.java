3c4337e56847c0250d76e90e318c36e5c4bc079e
Luis <802.250@alunos.unigran.br>
2022-09-23 19:14:11 -0400

    commit de criação da entidade

---
+++ app/src/main/java/br/unigran/p1/Consumo.java
@@ -0,0 +1,54 @@
+package br.unigran.p1;
+
+import java.util.Date;
+
+public class Consumo {
+    private Integer id, kmAtual, qtdAbastecida;
+    private Float valor;
+    private Date data;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getKmAtual() {
+        return kmAtual;
+    }
+
+    public void setKmAtual(Integer kmAtual) {
+        this.kmAtual = kmAtual;
+    }
+
+    public Integer getQtdAbastecida() {
+        return qtdAbastecida;
+    }
+
+    public void setQtdAbastecida(Integer qtdAbastecida) {
+        this.qtdAbastecida = qtdAbastecida;
+    }
+
+    public Float getValor() {
+        return valor;
+    }
+
+    public void setValor(Float valor) {
+        this.valor = valor;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return kmAtual + " " + qtdAbastecida + " " + valor + " " + data + "";
+    }
+}
